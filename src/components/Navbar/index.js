3a5ccdd27f97ba0fddfccdd312549fe00213983e
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 20:34:09 -0300

    navbar home

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+	return (
+		<AppBar position="static">
+			<Toolbar>
+				<Link className="navbar-link-home" to="/">
+					<Typography variant="h6">
+						Movie App
+				</Typography></Link>
+				<div className="navbar-container-link">
+					<Link>Películas</Link>
+					<Link>TV Shows</Link>
+					<Link>Personas</Link>
+				</div>
+			</Toolbar>
+		</AppBar>
+	)
+}

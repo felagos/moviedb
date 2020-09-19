0543a7282585b22497c1cc33cbddf16babbad5ce
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 16:35:54 -0300

    config y carga de genres

--- src/api/api.js
+++ src/api/api.js
@@ -3,5 +3,5 @@ import { ENV } from '../env';
 
 export const getGenres = async () => {
 	const response = await axios.get(`${ENV.API_URL}/genre/movie/list?language=es-ES`);
-	return response.data;
+	return response.data.genres;
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ENV } from '../env';
 
 axios.interceptors.request.use((config) => {
-	config.url = config.url.includes("=") ? `${config.url}&api_key=${ENV.API_KEY}` : `${config.url}?api_key=${API_KEY}`;
+	config.url = config.url.includes("=") ? `${config.url}&api_key=${ENV.API_KEY}` : `${config.url}?api_key=${ENV.API_KEY}`;
 	return config;
 }, (error) => {
 	return Promise.reject(error);

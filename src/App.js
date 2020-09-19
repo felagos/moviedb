0cce7d8a5dda734f7061e4624ad0d7e8529ed761
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 16:36:52 -0300

    provider redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { AppRouter } from './router/app-router';
+import { store } from './store';
 
 export const App = () => {
 	return (
-		<>
-		</>
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
 	);
 };
 

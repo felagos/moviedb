b1c557ec4b90427f224ce88d649c28fd63e3befb
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 16:15:11 -0300

    ocupo variable de entorno

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
-const API_KEY = process.env.REACT_APP_API_KEY;
+import { ENV } from '../env';
 
 axios.interceptors.request.use((config) => {
-	config.url = config.url.includes("=") ? `${config.url}&api_key=${API_KEY}` : `${config.url}?api_key=${API_KEY}`;
+	config.url = config.url.includes("=") ? `${config.url}&api_key=${ENV.API_KEY}` : `${config.url}?api_key=${API_KEY}`;
 	return config;
 }, (error) => {
 	return Promise.reject(error);

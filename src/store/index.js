d16067574ca374385a5c254846940ef8d3d304ce
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 21:11:36 -0300

    agrego movie reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { genreReducer } from '../reducers';
+import { genreReducer, movieReducer } from '../reducers';
 
 const composeEnhancers = window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] || compose;
 const middlwares = applyMiddleware(thunk);
 
 const reducers = combineReducers({
-	genre: genreReducer
+	genre: genreReducer,
+	movie: movieReducer
 });
 
 export const store = createStore(reducers, {}, composeEnhancers(middlwares));

78fa76833717a833ad1da4507205f6fdf80f2eba
Felipe Lagos <felagossanchez@gmail.com>
2020-09-20 14:38:49 -0300

    ruta para detalle movie

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { loadGenres, loadPopularMovies, loadTrendingMovies, loadUpcomingMovies } from '../actions';
-import { HomePage } from '../pages';
+import { HomePage, MovieDetail } from '../pages';
 
 export const AppRouter = () => {
 	const dispatch = useDispatch();
@@ -18,6 +18,7 @@ export const AppRouter = () => {
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
+				<Route exact path="/movie/:slug" component={MovieDetail} />
 			</Switch>
 		</BrowserRouter>
 	)

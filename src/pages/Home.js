885e8fbede92c0d3334e3ccf9568121b6dd370eb
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 22:28:05 -0300

    leer datos state redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { HomeLayout } from '../components/HomeLayout';
+import { ListMedia } from '../components/ListMedia';
 
 export const HomePage = () => {
+
+	const { popular, upcoming, trending } = useSelector(state => state.movie);
+
 	return (
 		<HomeLayout>
 			<div className="home-section">
 				<h2>Popular</h2>
+				<ListMedia media={popular} />
 			</div>
 			<div className="home-section">
 				<h2>Proximamente</h2>
+				<ListMedia media={upcoming} />
 			</div>
 			<div className="home-section">
 				<h2>Tendencias</h2>
+				<ListMedia media={trending} />
 			</div>
 		</HomeLayout>
 	)

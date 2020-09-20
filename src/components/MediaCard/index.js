bd75486cd9fd90fbc73916d402cbe93626c48af5
Felipe Lagos <felagossanchez@gmail.com>
2020-09-20 14:45:34 -0300

    item en state push

--- src/components/MediaCard/index.js
+++ src/components/MediaCard/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { castToMonthDay } from '../../helpers';
-import { useHistory, useLocation } from 'react-router-dom';
+import { castToMonthDay, slugUrl } from '../../helpers';
+import { useHistory } from 'react-router-dom';
 
 export const MediaCard = ({ item }) => {
 
 	const history = useHistory();
 
 	const goToDetail = () => {
-		history.push(`/movie/${item.id}`);
+		const url = `/movie/${slugUrl(item.id, item.original_title)}`;
+		alert(url);
+		history.push(url, item);
 	};
 
 	return (

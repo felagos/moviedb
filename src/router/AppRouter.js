7c8e765abb3e739bbf540839589da6d84eb1abfb
Felipe Lagos <felagossanchez@gmail.com>
2020-09-20 17:28:50 -0300

    quito llamada para obtener genres

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import { HomePage, MovieDetail } from '../pages';
 
 export const AppRouter = () => {
-
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route exact path="/movie/:slug" component={MovieDetail} />
+				<Route exact path="/movie/:id" component={MovieDetail} />
 			</Switch>
 		</BrowserRouter>
 	)

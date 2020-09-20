90b9150f872db23d5eb9efcb2bce176bb2c372b1
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 23:06:29 -0300

    componente media card

--- src/components/MediaCard/index.js
+++ src/components/MediaCard/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { castToMonthDay } from '../../helpers';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export const MediaCard = ({ item }) => {
+
+	const history = useHistory();
+
+	const goToDetail = () => {
+		history.push(`/movie/${item.id}`);
+	};
+
 	return (
-		<div className="media-card">
+		<div className="media-card" onClick={goToDetail}>
 			<img className="media-img-source" src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt={item.original_title} />
 			<div className="media-card-content">
 				<span className="media-title">{item.title}</span>

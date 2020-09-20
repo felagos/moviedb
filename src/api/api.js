aff2e294379789d55c24b309ed3c0c1c39a9aba0
Felipe Lagos <felagossanchez@gmail.com>
2020-09-19 21:26:45 -0300

    funciones para llamar a trending/upcoming/popular movies

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,21 @@ import axios from './axios';
 import { ENV } from '../env';
 
 export const getGenres = async () => {
-	const response = await axios.get(`${ENV.API_URL}/genre/movie/list?language=es-ES`);
+	const response = await axios.get(`${ENV.API_URL}/genre/movie/list`);
 	return response.data.genres;
+}
+
+export const getPopularMovies = async () => {
+	const response = await axios.get(`${ENV.API_URL}/movie/popular?page=1`);
+	return response.data.results;
+}
+
+export const getUpcomingMovies = async () => {
+	const response = await axios.get(`${ENV.API_URL}/movie/upcoming?page=1`);
+	return response.data.results;
+}
+
+export const getTrendingMovies = async () => {
+	const response = await axios.get(`${ENV.API_URL}/trending/movie/week`);
+	return response.data.results;
 }

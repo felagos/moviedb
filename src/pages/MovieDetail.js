85d0a8fe012992b301b6c142b0c478cfb1805831
Felipe Lagos <felagossanchez@gmail.com>
2020-09-20 14:39:07 -0300

    componente page para detalle movie

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { setActiveMovie } from '../actions';
 
 export const MovieDetail = () => {
-	const { id } = useParams();
-	const dispatch = useDispatch();
-	const { active } = useSelector(state => state.movie);
-
-	useEffect(() => {
-		dispatch(setActiveMovie());
-	}, [dispatch]);
+	const { state } = useLocation();
 
 	return (
 		<div>

bca3b2157aadb93fc7d81d7f072e69e86f480695
Felipe Lagos <felagossanchez@gmail.com>
2020-09-22 21:03:44 -0300

    llamada la busqueda

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { loadSearch } from '../redux';
 
 export const SearchPage = () => {
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const params = new URLSearchParams(location.search);
+
+	useEffect(() => {
+		dispatch(loadSearch(params.get("query")));
+	}, [dispatch, params]);
+
 	return (
 		<div>
 

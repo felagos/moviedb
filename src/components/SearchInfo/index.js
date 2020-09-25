2fecfad463bdf191b8c32126cbc932443a1ec85b
Felipe Lagos <felagossanchez@gmail.com>
2020-09-24 22:34:18 -0300

    menu info busqueda responsive

--- src/components/SearchInfo/index.js
+++ src/components/SearchInfo/index.js
@@ -28,23 +28,38 @@ export const SearchInfo = () => {
     }
 
     return (
-        <div className="search-info-container">
-            <div className="search-info-header">
-                <h4>Resultados de la búsqueda</h4>
+        <>
+            <div className="search-info-container">
+                <div className="search-info-header">
+                    <h4>Resultados de la búsqueda</h4>
+                </div>
+                <Paper elevation={1}>
+                    <List>
+                        <ListItem className="search-info-item" button onClick={handleFilterType("all")}>
+                            <span className="search-info-type">Todos</span> <Chip className="search-info-type-total" label={totals.totalMovies + totals.totalTvShows} />
+                        </ListItem>
+                        <ListItem className="search-info-item" button onClick={handleFilterType("movie")}>
+                            <span className="search-info-type">Peliculas</span> <Chip className="search-info-type-total" label={totals.totalMovies} />
+                        </ListItem>
+                        <ListItem button className="search-info-item" onClick={handleFilterType("type")}>
+                            <span className="search-info-type">TV Shows</span> <Chip className="search-info-type-total" label={totals.totalTvShows} />
+                        </ListItem>
+                    </List>
+                </Paper>
             </div>
-            <Paper elevation={1}>
-                <List>
+            <div className="search-info-container-mobile">
+                <List className="search-info-inline">
                     <ListItem className="search-info-item" button onClick={handleFilterType("all")}>
-                        <span className="search-info-type">Todos</span> <Chip className="search-info-type-total" label={totals.totalMovies + totals.totalTvShows} />
+                        <span className="search-info-type">Todos - {totals.totalMovies + totals.totalTvShows}</span>
                     </ListItem>
                     <ListItem className="search-info-item" button onClick={handleFilterType("movie")}>
-                        <span className="search-info-type">Peliculas</span> <Chip className="search-info-type-total" label={totals.totalMovies} />
+                        <span className="search-info-type">Peliculas - {totals.totalMovies}</span>
                     </ListItem>
-                    <ListItem button className="search-info-item" onClick={handleFilterType("type")}>
-                        <span className="search-info-type">TV Shows</span> <Chip className="search-info-type-total" label={totals.totalTvShows} />
+                    <ListItem className="search-info-item"button onClick={handleFilterType("type")}>
+                        <span className="search-info-type">TV Shows - {totals.totalTvShows}</span>
                     </ListItem>
                 </List>
-            </Paper>
-        </div>
+            </div>
+        </>
     )
 }
